f5e7cb920f947430c8985e82c0a134e237eb3d2b
Abelsaba06 <abelsabatermunoz@gmail.com>
2025-09-23 07:46:48 +0000

    Hola

---
+++ src/Controller/PageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+final class PageController extends AbstractController
+{
+    #[Route('/page', name: 'app_page')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/PageController.php',
+        ]);
+    }
+    #[Route('/',name:'inicio')]
+    public function inicio():Response 
+    {
+        return new Response("Bienvenido a la pagina web de contactos");
+    }
+}
