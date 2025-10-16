93117b27c746f326493477a77e90d9ea3b4220dc
Abelsaba06 <abelsabatermunoz@gmail.com>
2025-10-16 08:15:37 +0000

    UserRepository corregido

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -9,14 +9,24 @@ use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
-use Symfony\Bridge\Doctrine\Security\User\UserLoaderIdentifier;
-
 
 /**
  * @extends ServiceEntityRepository<User>
  */
 class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
+    public function loadUserByIdentifier(string $usernameOrEmail): ?User
+    {
+        $entityManager = $this->getEntityManager();
+        return $entityManager->createQuery(
+            'SELECT u
+                FROM App\Entity\User u
+                WHERE u.email = :query
+                OR u.username = :query' 
+        )
+            ->setParameter('query', $usernameOrEmail)
+            ->getOneOrNullResult();
+        }
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
@@ -35,18 +45,6 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $this->getEntityManager()->persist($user);
         $this->getEntityManager()->flush();
     }
-
-    public function loadUserByIdentifier(string $usernameOrEmail): ?User
-    {
-        $entityManager = $this->getEntityManager();
-        return $entityManager->createQuery(
-            'SELECT u
-                FROM App\Entity\User u
-                WHERE u.email = :query
-                OR u.username = :query' 
-        )
-            ->setParameter('query', $usernameOrEmail)
-            ->getOneOrNullResult();
 
     //    /**
     //     * @return User[] Returns an array of User objects
